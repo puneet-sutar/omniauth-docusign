d7e113d5dc9d2aba21b8a15d7399bd043ff79673
puneetsutar <puneet@bigbinary.com>
2017-12-17 13:55:21 -0500

    Fixed specs

--- spec/omniauth/docusign_spec.rb
+++ spec/omniauth/docusign_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Omniauth::Docusign do
   it "has a version number" do
     expect(Omniauth::Docusign::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "omniauth/docusign"
+require "omniauth/strategies/docusign"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
